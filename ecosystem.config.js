4610e35e53dfbf4c5c81fa234e4800ca7d83333e
Chanuth Dewhan <chanuthdewhan273@gmail.com>
2026-08-27 20:35:06 +0530

    fix: incorrect jar file names fixed

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,17 +2,17 @@ module.exports = {
   apps : [
     {
       name   : "config-server",
-      script : "java -jar ./config-server/target/Config-Server-1.0.0.jar",
+      script : "java -jar ./config-server/target/config-server-1.0.0.jar",
       log_file: "./logs/config-server.log"
     },
     {
       name   : "service-registry",
-      script : "java -jar ./service-registry/target/Service-Registry-1.0.0.jar",
+      script : "java -jar ./service-registry/target/service-registry-1.0.0.jar",
       log_file: "./logs/service-registry.log"
     },
     {
       name   : "api-gateway",
-      script : "java -jar ./api-gateway/target/Api-Gateway-1.0.0.jar",
+      script : "java -jar ./api-gateway/target/api-gateway-1.0.0.jar",
       log_file: "./logs/api-gateway.log"
     }
   ]
